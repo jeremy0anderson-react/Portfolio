db1b1c63ef4e83e1e08efaa6182586e36d554884
Jeremy Anderson <jeremy.danderson@outlook.com>
2022-07-05 16:39:08 -0600

    fixed directory mapping

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ io.on('connection', (socket)=>{
 
 
 app.get("*", (req, res)=>{
-    res.sendFile(path.join(__dirname, "portfolio","build","index.html"))
+    res.sendFile(path.join(__dirname, "./portfolio","build","index.html"))
 })
 
 httpServer.listen(PORT, ()=>{
