b3f5d6f4ee4b556bca036b357426d3c2b2a0afa9
Jeremy Anderson <jeremy.danderson@outlook.com>
2022-07-05 13:33:03 -0600

    heroku config for react app w/server

--- portfolio/src/context/socket.js
+++ portfolio/src/context/socket.js
@@ -1,7 +1,7 @@
 import {io} from 'socket.io-client';
 import {createContext} from "react";
 
-export const socket=io("http://localhost:4000",{
+export const socket=io("https://react-port-final.herokuapp.com/",{
     transports: ['websocket', 'polling'],
     autoConnect: false
 });
